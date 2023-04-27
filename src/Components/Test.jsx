17f668537a2d062eccfff257239c67437a8bda73
Prashantp9 <prashantdsk75@gmail.com>
2023-04-27 18:03:06 +0530

    input

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -1,35 +1,39 @@
-import react, { useState } from "react"
-import {planeWordList} from "../WordList/planewordlist"
+import react, { useState } from "react";
+import { planeWordList } from "../WordList/planewordlist";
 
-const Test =()=>{ 
-const[currWord,setCurrWord]= useState(0)
-const[currWordStatus,setCurrWordStatus] = useState(false)
+const Test = () => {
+  const [currWord, setCurrWord] = useState(0);
+  const [currWordStatus, setCurrWordStatus] = useState(false);
 
-
-
-const handlekeyUp =(event)=>{
-    if(event.key == ' ') {
-        setCurrWord(currWord + 1)
+  const handlekeyUp = (event) => {
+    if (event.key == " ") {
+      event.target.value = " ";
+      setCurrWord(currWord + 1);
     }
-}
+  };
 
-  const wordListArray = planeWordList.split(" ")
+  const wordListArray = planeWordList.split(" ");
 
-const handleConrrection =(e)=>{
-    let currIndex = e.length
-    if(wordListArray[currWord]?.slice(0,currIndex) !== e){
-        setCurrWordStatus(true)
+  const handleConrrection = (e) => {
+    let currIndex = e.length;
+    if (wordListArray[currWord]?.slice(0, currIndex) !== e) {
+      setCurrWordStatus(true);
+    } else {
+      setCurrWordStatus(false);
     }
-    else{
-        setCurrWordStatus(false)
-    }
-  }
- return(
-<>
-  <h1 style={{color: currWordStatus ? "red" : "black"}}>{wordListArray[currWord]}</h1>
-  <input type="text" onChange={(e)=>handleConrrection(e.target.value)} onKeyUp={handlekeyUp}/>
-</>
- )   
-}
+  };
+  return (
+    <>
+      <h1 style={{ color: currWordStatus ? "red" : "black" }}>
+        {wordListArray[currWord]}
+      </h1>
+      <input
+        type="text"
+        onChange={(e) => handleConrrection(e.target.value)}
+        onKeyUp={handlekeyUp}
+      />
+    </>
+  );
+};
 
-export default Test
+export default Test;
