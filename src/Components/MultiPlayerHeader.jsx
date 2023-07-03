4bc3908c96cd7326d037d7fed7525b0ad4ce1222
Prashantp9 <prashantdsk75@gmail.com>
2023-07-03 13:17:08 +0530

    changes in application

--- src/Components/MultiPlayerHeader.jsx
+++ src/Components/MultiPlayerHeader.jsx
@@ -1,6 +1,6 @@
 import "../StyleSheets/MultiPlayerHeader.css";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import useSocketHook, { socket } from "../customHooks/useSetupHook.js";
 
@@ -8,27 +8,69 @@ import useSocketroom from "../customHooks/useSocketroom.js";
 
 const CreateRoomContainer = (props) => {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
   const setRoomParams = () => {
     useSocketroom.createRoom(socket.id + "room");
     navigate(`fast_fingers/${socket.id + "room"}`);
   };
+
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(socket?.id + "room")
+      .then(() => {
+        alert("text copied to clipboard");
+      })
+      .catch((error) => {
+        console.error("Error copying text to clipboard:", error);
+      });
+  };
+
   return (
-    <>
+    <div className="create-room-bg-container" onClick={(e) => props.onclick(e)}>
       <div className="create-rooom-component">
-        <div className="link-container">{socket.id + "room"}</div>
-        <button onClick={() => setRoomParams()}>Create Room</button>
-        <button>Copy Link</button>
+        <div className="link-container-outer-div">
+          {socket.id ? (
+            <p
+              className="link-container"
+              style={{ cursor: socket.id ? "pointer" : "wait" }}
+            >
+              {socket.id + "room"}
+            </p>
+          ) : (
+            <p
+              className="link-container"
+              style={{ cursor: socket.id ? "pointer" : "wait" }}
+            >
+              Server Loading....
+            </p>
+          )}
+        </div>
+
+        <button
+          onClick={() => setRoomParams()}
+          disabled={!socket.id}
+          style={{ cursor: socket.id ? "pointer" : "not-allowed" }}
+        >
+          Create Room
+        </button>
+
+        <button
+          onClick={() => copyToClipboard()}
+          style={{ cursor: socket.id ? "pointer" : "not-allowed" }}
+          disabled={!socket.id}
+        >
+          {" "}
+          Copy Link
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
-const JoinRoomContainer = () => {
+const JoinRoomContainer = (props) => {
   const navigate = useNavigate();
   const [joiningLink, setJoiningLink] = useState("");
   return (
-    <>
+    <div className="create-room-bg-container" onClick={(e) => props.onclick(e)}>
       <div className="create-rooom-component">
         <input
           placeholder="enter joining link here"
@@ -46,9 +88,10 @@ const JoinRoomContainer = () => {
           Join Room
         </button>
       </div>
-    </>
+    </div>
   );
 };
+
 const MultiPlayerHeader = () => {
   const [createRoom, setCreateRoom] = useState(false);
   const [joinRoom, setJoinRoom] = useState(false);
@@ -57,6 +100,17 @@ const MultiPlayerHeader = () => {
 
   return (
     <>
+      {createRoom && (
+        <CreateRoomContainer
+          onclick={(e) => e.currentTarget == e.target && setCreateRoom(false)}
+          socketId={socketId}
+        />
+      )}
+      {joinRoom && (
+        <JoinRoomContainer
+          onclick={(e) => e.currentTarget == e.target && setJoinRoom(false)}
+        />
+      )}
       <div className="create-room-outer-container">
         <div className="create-room-main-container">
           <div className="room-optin-container">
@@ -65,14 +119,12 @@ const MultiPlayerHeader = () => {
               onClick={() => setCreateRoom(!createRoom)}
             >
               Create
-              {createRoom && <CreateRoomContainer socketId={socketId} />}
             </div>
             <div
               className="join-room-container"
               onClick={() => setJoinRoom(true)}
             >
               Join
-              {joinRoom && <JoinRoomContainer />}
             </div>
           </div>
         </div>
